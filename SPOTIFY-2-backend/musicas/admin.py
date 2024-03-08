8a4b2414c5a5ee0fa81c5d91e0d1a86ffe95138a
levzxn <levilllzac@gmail.com>
2024-03-08 14:45:30 -0300

    ASDFAS

--- SPOTIFY-2-backend/musicas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from musicas.models import Musicas
-
-class MusicasAdmin(admin.ModelAdmin):
-    list_display = ('titulo',)
-
-admin.site.register(Musicas,MusicasAdmin)
-
